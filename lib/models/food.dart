dc7769c8887e3682eca805d537271eef85899a2f
unknown <sazid.khandaker@northsouth.edu>
2024-07-07 14:29:59 +0600

    food class  done

--- lib/models/food.dart
+++ lib/models/food.dart
@@ -3,10 +3,23 @@ class Food {
   final String description;
   final String imagepath;
   final int price;
+  final Foodcategory category;
+  List<Addone> availableAddOne;
 
   Food(
       {required this.name,
       required this.description,
       required this.imagepath,
-      required this.price});
+      required this.price,
+      required this.category,
+      required this.availableAddOne});
+}
+
+enum Foodcategory { burgers, dessert, drinks, salad, sider }
+
+class Addone {
+  final String name;
+  final int price;
+
+  Addone({required this.name, required this.price});
 }
