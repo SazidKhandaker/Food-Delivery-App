a4778b31909b15ede5536f0075fcd9eaaf9da1b1
unknown <sazid.khandaker@northsouth.edu>
2024-07-07 14:22:59 +0600

    food class  processing

---
+++ lib/models/food.dart
@@ -0,0 +1,12 @@
+class Food {
+  final String name;
+  final String description;
+  final String imagepath;
+  final int price;
+
+  Food(
+      {required this.name,
+      required this.description,
+      required this.imagepath,
+      required this.price});
+}
