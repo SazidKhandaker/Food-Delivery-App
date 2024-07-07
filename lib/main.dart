70bab9d7a6d1415852814ca92ceb99ada4700ec9
unknown <sazid.khandaker@northsouth.edu>
2024-07-07 23:40:26 +0600

    Changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/authentication/loginpage.dart';
 import 'package:food_delivery_app/authentication/registerorlogin.dart';
