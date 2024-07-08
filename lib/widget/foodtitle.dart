6133a59ff1cd5b040d50e7236711379cf983dd19
unknown <sazid.khandaker@northsouth.edu>
2024-07-08 22:01:44 +0600

    Food title

--- lib/widget/foodtitle.dart
+++ lib/widget/foodtitle.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/models/food.dart';
 
+class MyFoodTitle extends StatelessWidget {
+  final Food food;
+  final void Function()? ontap;
+  const MyFoodTitle({required this.food, required this.ontap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+            child: Column(
+          children: [
+            Text("${food.name}"),
+            Text("${food.price}"),
+            Text("${food.description}")
+          ],
+        )),
+        Image.asset(
+          food.imagepath,
+          height: 120,
+          width: 120,
+        ),
+      ],
+    );
+  }
+}
