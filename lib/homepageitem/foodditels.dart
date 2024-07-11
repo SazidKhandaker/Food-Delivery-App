23af0a6b8998c64d37998fa192d525b3f195b2bd
unknown <sazid.khandaker@northsouth.edu>
2024-07-11 21:14:03 +0600

    Food  page ditels Added with name anddiscription

--- lib/homepageitem/foodditels.dart
+++ lib/homepageitem/foodditels.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/models/food.dart';
 
-class FoodPageDitels extends StatelessWidget {
+class FoodPageDitels extends StatefulWidget {
   final Food food;
 
   FoodPageDitels({required this.food});
 
   @override
+  State<FoodPageDitels> createState() => _FoodPageDitelsState();
+}
+
+class _FoodPageDitelsState extends State<FoodPageDitels> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Image.asset("${food.imagepath}"),
+          Image.asset("${widget.food.imagepath}"),
+          Text("${widget.food.name}"),
+          SizedBox(
+            height: 10,
+          ),
+          Text("${widget.food.description}}"),
+          SizedBox(
+            height: 10,
+          ),
         ],
       ),
     );
