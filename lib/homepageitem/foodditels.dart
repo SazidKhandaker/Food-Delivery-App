fca0101e805e1dec31e36979d93c4dfbee916868
unknown <sazid.khandaker@northsouth.edu>
2024-07-13 22:37:22 +0600

    Food  page ditels appbar added

--- lib/homepageitem/foodditels.dart
+++ lib/homepageitem/foodditels.dart
@@ -19,101 +19,127 @@ class FoodPageDitels extends StatefulWidget {
 class _FoodPageDitelsState extends State<FoodPageDitels> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset("${widget.food.imagepath}"),
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "${widget.food.name}",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
+    return Stack(
+      children: [
+        Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Image.asset("${widget.food.imagepath}"),
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${widget.food.name}",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "${widget.food.price} Taka",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color:
+                                Theme.of(context).colorScheme.inversePrimary),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "${widget.food.description}",
+                        style: TextStyle(
+                            fontSize: 16,
+                            color:
+                                Theme.of(context).colorScheme.inversePrimary),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Divider(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Add-one",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color:
+                                Theme.of(context).colorScheme.inversePrimary),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Theme.of(context).colorScheme.secondary),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: ListView.builder(
+                          itemCount: widget.food.availableAddOne.length,
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            Addone addone = widget.food.availableAddOne[index];
+                            return CheckboxListTile(
+                                title: Text("${addone.name}"),
+                                subtitle: Text(
+                                  "${addone.price} Taka",
+                                  style: TextStyle(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .inversePrimary),
+                                ),
+                                value: widget.selectAvaiableAddons[addone],
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    widget.selectAvaiableAddons[addone] =
+                                        value!;
+                                  });
+                                });
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "${widget.food.price} Taka",
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.inversePrimary),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "${widget.food.description}",
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: Theme.of(context).colorScheme.inversePrimary),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Divider(
-                    color: Theme.of(context).colorScheme.secondary,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Add-one",
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.inversePrimary),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                          color: Theme.of(context).colorScheme.secondary),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: ListView.builder(
-                      itemCount: widget.food.availableAddOne.length,
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        Addone addone = widget.food.availableAddOne[index];
-                        return CheckboxListTile(
-                            title: Text("${addone.name}"),
-                            subtitle: Text(
-                              "${addone.price} Taka",
-                              style: TextStyle(
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .inversePrimary),
-                            ),
-                            value: widget.selectAvaiableAddons[addone],
-                            onChanged: (bool? value) {
-                              setState(() {
-                                widget.selectAvaiableAddons[addone] = value!;
-                              });
-                            });
-                      },
-                    ),
-                  ),
-                ],
+                ),
+                myButton(ontap: () {}, buttonname: "Add To Cart"),
+                SizedBox(
+                  height: 25,
+                )
+              ],
+            ),
+          ),
+        ),
+        SafeArea(
+          child: Opacity(
+            opacity: 0.6,
+            child: Container(
+              margin: EdgeInsets.only(left: 10, top: 10),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              child: IconButton(
+                icon: Icon(Icons.arrow_back_ios_new),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
             ),
-            myButton(ontap: () {}, buttonname: "Add To Cart"),
-            SizedBox(
-              height: 25,
-            )
-          ],
-        ),
-      ),
+          ),
+        )
+      ],
     );
   }
 }
